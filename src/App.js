3bc7e89cd5f169bff1a38379b4b7c873804173c5
dhanashri <surpuredhanashri@gmail.com>
2021-02-02 19:33:02 +0530

    update checkout button

--- src/App.js
+++ src/App.js
@@ -5,65 +5,76 @@ import Filter from "./components/Filter";
 import Cart from "./components/Cart";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      products: data.products,
-      cartItems: [],
-      size: "",
-      sort: "",
-    };
+	constructor() {
+		super();
+		this.state = {
+			products: data.products,
+			cartItems: localStorage.getItem("cartItems")
+				? JSON.parse(localStorage.getItem("cartItems"))
+				: [],
+			size: "",
+			sort: "",
+		};
   }
-  removeFromCart = (product) => {
-    const cartItems = this.state.cartItems.slice();
-    this.setState({
-      cartItems: cartItems.filter((x) => x._id !== product._id),
-    });
+  createOrder = (order) => {
+    alert("Need to save order for " + order.name);
   };
-  addToCart = (product) => {
-    const cartItems = this.state.cartItems.slice();
-    let alreadyInCart = false;
-    cartItems.forEach((item) => {
-      if (item._id === product._id) {
-        item.count++;
-        alreadyInCart = true;
-      }
+
+	removeFromCart = (product) => {
+		const cartItems = this.state.cartItems.slice();
+		this.setState({
+			cartItems: cartItems.filter((x) => x._id !== product._id),
     });
-    if (!alreadyInCart) {
-      cartItems.push({ ...product, count: 1 });
-    }
+    localStorage.setItem(
+      "cartItems",
+      JSON.stringify(cartItems.filter((x) => x._id !== product._id))
+    );
+	};
+	addToCart = (product) => {
+		const cartItems = this.state.cartItems.slice();
+		let alreadyInCart = false;
+		cartItems.forEach((item) => {
+			if (item._id === product._id) {
+				item.count++;
+				alreadyInCart = true;
+			}
+		});
+		if (!alreadyInCart) {
+			cartItems.push({ ...product, count: 1 });
+		}
     this.setState({ cartItems });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
   };
   
-  render() {
-    return (
-      <div className="grid-container">
-        <header>
-          <a href="/">React Shopping Cart</a>
-        </header>
-        <main>
-          <div className="content">
-            <div className="main">
-            <Filter
-                count={this.state.products.length}
-            ></Filter>
-                <Products
-                products={this.state.products}
-                addToCart={this.addToCart}
-              ></Products>
-            </div>
-            <div className="sidebar">
-              <Cart
-                cartItems={this.state.cartItems}
+
+	render() {
+		return (
+			<div className="grid-container">
+				<header>
+					<a href="/">React Shopping Cart</a>
+				</header>
+				<main>
+					<div className="content">
+						<div className="main">
+							<Filter count={this.state.products.length}></Filter>
+							<Products
+								products={this.state.products}
+								addToCart={this.addToCart}
+							></Products>
+						</div>
+						<div className="sidebar">
+							<Cart
+								cartItems={this.state.cartItems}
                 removeFromCart={this.removeFromCart}
-              />
-            </div>
-          </div>
-        </main>
-        <footer>All right is reserved.</footer>
-      </div>
-    );
-  }
+                createOrder={this.createOrder}
+							/>
+						</div>
+					</div>
+				</main>
+				<footer>All right is reserved.</footer>
+			</div>
+		);
+	}
 }
 
 export default App;
